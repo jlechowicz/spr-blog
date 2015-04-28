fa137794b7dd90cc8c2f3069f6b68ab1bb7bbc10
jlechowicz <jlechowicz@gmail.com>
2015-04-27 22:54:32 -0500

    Added Tag table

---
+++ BlogDatabase/Tables/Tag.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Tag]
+(
+	[Id] INT NOT NULL  IDENTITY, 
+    [TagName] VARCHAR(50) NOT NULL, 
+    CONSTRAINT [PK_Tag] PRIMARY KEY ([Id]),
+	CONSTRAINT [U_TagName] UNIQUE([TagName])
+)

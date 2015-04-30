4200aa7899ab01b35a3edc2c051c4be7d4e6c0eb
jlechowicz <jlechowicz@gmail.com>
2015-04-29 22:38:47 -0500

    Added PostTag table

---
+++ BlogDatabase/Tables/PostTag.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[PostTag]
+(
+	[PostId] INT NOT NULL, 
+    [TagId] INT NOT NULL, 
+    PRIMARY KEY ([TagId], [PostId]), 
+    CONSTRAINT [FK_PostTag_Post] FOREIGN KEY ([PostId]) REFERENCES [Post]([Id]),
+	CONSTRAINT [FK_PostTag_Tag] FOREIGN KEY ([TagId]) REFERENCES [Tag]([Id]) 
+)

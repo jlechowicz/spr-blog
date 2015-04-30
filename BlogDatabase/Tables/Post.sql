6b3f3577f5ac7843189b44e08ec2f8a7c100a0ec
jlechowicz <jlechowicz@gmail.com>
2015-04-29 22:30:13 -0500

    Added Post table

---
+++ BlogDatabase/Tables/Post.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Post]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [AuthorId] INT NOT NULL, 
+	[IsPublic] BIT NOT NULL DEFAULT 0,
+    [IsDeleted] BIT NOT NULL DEFAULT 0, 
+    [CreatedOn] DATETIME2 NOT NULL DEFAULT GetDate(), 
+    [LastModified] DATETIME2 NULL
+)

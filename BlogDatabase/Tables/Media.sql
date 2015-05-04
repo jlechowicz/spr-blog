b4d1feb63a5f55bacf9b81c202a077a24c36cda4
jlechowicz <jlechowicz@gmail.com>
2015-05-04 18:05:22 -0500

    Added Media table

---
+++ BlogDatabase/Tables/Media.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Media]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Path] VARCHAR(1000) NULL, 
+    [MediaTypeId] INT NULL, 
+    [Name] VARCHAR(50) NULL, 
+    [IsDeleted] BIT NOT NULL DEFAULT 0, 
+    [CreatedOn] DATETIME2 NOT NULL DEFAULT GetDate()
+)

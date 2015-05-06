3c003d50eceb31456a97f0249ba2e1659e033a28
jlechowicz <jlechowicz@gmail.com>
2015-05-06 18:01:34 -0500

    Added IUnitOfWork interface

---
+++ BlogDataService/Interface/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogDataService.Interface
+{
+    public interface IUnitOfWork
+    {
+        void Commit();
+        void Rollback();
+        T Fetch<T>(object key);
+    }
+}

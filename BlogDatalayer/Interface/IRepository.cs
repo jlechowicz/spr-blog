7f41879b29efb04a96d40596a593e691ede5e68a
jlechowicz <jlechowicz@gmail.com>
2015-05-06 18:10:59 -0500

    Added methods to IRepository

--- BlogDatalayer/Interface/IRepository.cs
+++ BlogDatalayer/Interface/IRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,14 @@ namespace BlogDatalayer.Interface
 {
     public interface IRepository
     {
+        void Save();
+        void Rollback();
+        void Add<T>(T Entity);
+        void Update<T>(T Entity);
+        void Delete<T>(T Entity);
+        void Attach<T>(T Entity);
+        void Detatch<T>(T Entity);
+        T Find<T>(object key);
+        IQueryable<T> All<T>();
     }
 }

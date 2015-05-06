89cb3d3b8d98fe47baf7bf6a76d3ee920745f455
jlechowicz <jlechowicz@gmail.com>
2015-05-06 18:48:21 -0500

    Added first test file

---
+++ BlogTests/DataServiceTests/RepositoryTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BlogTests.DataServiceTests
+{
+    [TestClass]
+    public class RepositoryTests
+    {
+        [TestMethod]
+        public void Test()
+        {
+
+        }
+    }
+}

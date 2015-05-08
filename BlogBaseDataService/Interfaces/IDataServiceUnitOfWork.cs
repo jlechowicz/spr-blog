02d9d63aa3baf465e394af183b9e551bf0b68043
jlechowicz <jlechowicz@gmail.com>
2015-05-07 20:14:38 -0500

    added IDataServiceUnitOfWork

---
+++ BlogBaseDataService/Interfaces/IDataServiceUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogBaseDataService.Interfaces
+{
+    public interface IDataServiceUnitOfWork
+    {
+        IDataAccessService DataAccessService { get; }
+        T ResolveDataService<T>() where T : IDataServiceInitialize;
+        void Save();
+        void Update<T>(T entity) where T : class;
+    }
+}

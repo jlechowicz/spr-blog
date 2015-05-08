5fc62e08fa0f1ac1dc46a64b5a6e8064b08f8878
jlechowicz <jlechowicz@gmail.com>
2015-05-07 20:11:29 -0500

    Added IDataServiceInitialize

---
+++ BlogBaseDataService/Interfaces/IDataServiceInitialize.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogBaseDataService.Interfaces
+{
+    public interface IDataServiceInitialize : IDataService
+    {
+        void Initialize(IDataAccessService dataAccessService);
+    }
+}

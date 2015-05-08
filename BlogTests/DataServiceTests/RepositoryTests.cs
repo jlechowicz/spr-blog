b8477e1faf4b70e3335a54bbb6bc4291bd9297e2
jlechowicz <jlechowicz@gmail.com>
2015-05-08 15:18:01 -0500

    Added references to use data service in Test project

--- BlogTests/DataServiceTests/RepositoryTests.cs
+++ BlogTests/DataServiceTests/RepositoryTests.cs
@@ -1,5 +1,11 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BlogBaseDataService.Implementation;
+using Microsoft.Practices.ServiceLocation;
+using BlogBaseDataService.Interfaces;
+using BlogDatalayer;
+using System.Linq;
+
 
 namespace BlogTests.DataServiceTests
 {
@@ -9,7 +15,9 @@ namespace BlogTests.DataServiceTests
         [TestMethod]
         public void Test()
         {
-
+            var UoW = ServiceLocator.Current.GetInstance<IDataServiceUnitOfWork>();
+            var posts = UoW.DataAccessService.All<Post>();
+            Assert.AreEqual(0, posts.Count());
         }
     }
 }

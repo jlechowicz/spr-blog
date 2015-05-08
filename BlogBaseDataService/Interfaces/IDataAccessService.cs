598fd520f5879a3392ec5f09587339ebc29260d2
jlechowicz <jlechowicz@gmail.com>
2015-05-07 20:09:12 -0500

    Added IDataService

--- BlogBaseDataService/Interfaces/IDataAccessService.cs
+++ BlogBaseDataService/Interfaces/IDataAccessService.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace BlogBaseDataService.Interfaces
 {
-    interface IDataAccessService
+    public interface IDataAccessService : IDisposable
     {
+        void Add<TEntity>(TEntity entity) where TEntity : class;
+        void Delete<TEntity>(TEntity entity) where TEntity : class;
+        void Update<TEntity>(TEntity entity) where TEntity : class;
+        TEntity GetById<TEntity>(long id) where TEntity : class;
+        TEntity GetById<TEntity>(Guid id) where TEntity : class;
+        void DeleteById<TEntity>(long id) where TEntity : class;
+        bool IsEntityModified<TEntity>(TEntity entity) where TEntity : class;
+        IQueryable<TEntity> All<TEntity>() where TEntity : class;
+        IQueryable<TEntity> NoTracking<TEntity>() where TEntity : class;
+        void SaveChanges();
+        int ExecuteSqlCommand(string sql, params object[] parameters);
+        int ExecuteSqlCommand(TimeSpan timeOut, string sql, params object[] parameters);
+        IEnumerable<T> ExecuteStoredProc<T>(string spName, params object[] parameters);
     }
 }

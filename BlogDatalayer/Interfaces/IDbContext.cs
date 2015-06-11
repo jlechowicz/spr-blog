cab262168c21c63624f068ba8b2a7519cc24fd2c
John Lechowicz <jlechowicz@gmail.com>
2015-06-11 15:30:37 -0500

    Renamed interface IRepository to IDbContext

---
+++ BlogDatalayer/Interfaces/IDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogDatalayer.Interfaces
+{
+    public interface IDbContext
+    {
+        void Save();
+        void Create<EntityType>() where EntityType : class;
+        void Add<EntityType>(EntityType Entity) where EntityType : class;
+        void Update<EntityType>(EntityType Entity) where EntityType : class;
+        void Delete<EntityType>(EntityType Entity) where EntityType : class;
+        void Attach<EntityType>(EntityType Entity) where EntityType : class;
+        void Detatch<EntityType>(EntityType Entity) where EntityType : class;
+        EntityType Find<EntityType>(params object[] keys) where EntityType : class;
+        IQueryable<EntityType> All<EntityType>() where EntityType : class;
+    }
+}

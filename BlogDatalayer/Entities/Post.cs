6aabf821072e551a2a1e77dcf1c8f7f75b15c5c6
John Lechowicz <jlechowicz@gmail.com>
2015-06-11 15:58:16 -0500

    Added Post entity

---
+++ BlogDatalayer/Entities/Post.cs
@@ -0,0 +1,18 @@
+﻿using BlogDatalayer.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogDatalayer.Entities
+{
+    public class Post : DataEntityBase
+    {
+        public int AuthorId { get; set; }
+        public bool IsPublic { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime? LastModified { get; set; }
+    }
+}

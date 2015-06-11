fbf30f684469f4caf1c7bb65808d323daf744893
John Lechowicz <jlechowicz@gmail.com>
2015-06-11 15:51:50 -0500

    Added abstract entity types

---
+++ BlogDatalayer/Abstracts/DualPrimaryKeyEntityBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogDatalayer.Abstracts
+{
+    public class DualPrimaryKeyEntityBase
+    {
+        private int _primaryKey1;
+        public int PrimaryKey1 { get { return _primaryKey1; } }
+        private int _primaryKey2;
+        public int PrimaryKey2 { get { return _primaryKey2; } }
+    }
+}

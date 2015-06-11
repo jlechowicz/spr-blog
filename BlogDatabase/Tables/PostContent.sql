ebc798125da5c8e7e17c2e65e94628ae2ea0e5b8
John Lechowicz <jlechowicz@gmail.com>
2015-06-11 15:45:19 -0500

    Changed column data type

--- BlogDatabase/Tables/PostContent.sql
+++ BlogDatabase/Tables/PostContent.sql
@@ -3,7 +3,7 @@
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
     [PostId] INT NOT NULL, 
     [Title] VARCHAR(50) NOT NULL DEFAULT '(Untitled)', 
-    [Content] TEXT NULL, 
+    [Content] VARCHAR(MAX) NULL, 
     [SavedOn] DATETIME2 NOT NULL DEFAULT GetDate(), 
     [IsDisplayed] BIT NOT NULL DEFAULT 0, 
     [EditedBy] INT NOT NULL,

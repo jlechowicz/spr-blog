09a5aefb1cec79989c56cc86851be891050b0228
John Lechowicz <jlechowicz@gmail.com>
2015-06-11 16:11:39 -0500

    Added PostContent entity

---
+++ BlogDatalayer/Entities/PostContent.cs
@@ -0,0 +1,19 @@
+﻿using BlogDatalayer.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogDatalayer.Entities
+{
+    public class PostContent : DataEntityBase
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime SavedOn { get; set; }
+        public bool IsDisplayed { get; set; }
+        public int EditedBy { get; set; }
+    }
+}

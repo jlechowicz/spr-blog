278234243d973200c81988ac268d2b74d8914fcb
John Lechowicz <jlechowicz@gmail.com>
2015-06-13 02:19:06 -0500

    Added FK Constraints for User table

--- BlogDatabase/Tables/Post.sql
+++ BlogDatabase/Tables/Post.sql
@@ -5,5 +5,6 @@
 	[IsPublic] BIT NOT NULL DEFAULT 0,
     [IsDeleted] BIT NOT NULL DEFAULT 0, 
     [CreatedOn] DATETIME2 NOT NULL DEFAULT GetDate(), 
-    [LastModified] DATETIME2 NULL
+    [LastModified] DATETIME2 NULL,
+	CONSTRAINT [FK_Post_User] FOREIGN KEY ([AuthorId]) REFERENCES [User]([Id])
 )
--- BlogDatabase/Tables/PostContent.sql
+++ BlogDatabase/Tables/PostContent.sql
@@ -7,5 +7,6 @@
     [SavedOn] DATETIME2 NOT NULL DEFAULT GetDate(), 
     [IsDisplayed] BIT NOT NULL DEFAULT 0, 
     [EditedBy] INT NOT NULL,
-	CONSTRAINT [FK_PostContent_Post] FOREIGN KEY ([PostId]) REFERENCES [Post]([Id]) 
+	CONSTRAINT [FK_PostContent_Post] FOREIGN KEY ([PostId]) REFERENCES [Post]([Id]),
+	CONSTRAINT [FK_PostContent _User] FOREIGN KEY ([EditedBy]) REFERENCES [User]([Id])
 )

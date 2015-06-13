66ac48905c93c35e943c3c9cc6a9782dc667082b
John Lechowicz <jlechowicz@gmail.com>
2015-06-13 17:04:13 -0500

    Added MimeType table

---
+++ BlogDatabase/Tables/MimeType.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[MimeType]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+	[Definition] VARCHAR(250) NOT NULL,
+	[AssociatedFileExtensions] VARCHAR(MAX) NOT NULL,
+	[IsActive] BIT NOT NULL DEFAULT 0,
+	CONSTRAINT [U_MimeType_Definition] UNIQUE([Definition])
+)

2b9c8a8fe7a1dad5976d0d3cd16654607c669f36
John Lechowicz <jlechowicz@gmail.com>
2015-06-13 02:13:20 -0500

    Added UserRole table

---
+++ BlogDatabase/Tables/UserRole.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[UserRole]
+(
+	[UserId] INT NOT NULL,
+	[RoleId] INT NOT NULL,
+	PRIMARY KEY ([UserId], [RoleId]),
+	CONSTRAINT [FK_User_UserRole] FOREIGN KEY ([UserId]) REFERENCES [User]([Id]),
+	CONSTRAINT [FK_Role_UserRole] FOREIGN KEY ([RoleId]) REFERENCES [Role]([Id])
+)

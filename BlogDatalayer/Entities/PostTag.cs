4e8370b6587dc3b26868e2c04584bc9603074429
John Lechowicz <jlechowicz@gmail.com>
2015-06-13 01:46:00 -0500

    Removed DuelPrimaryKeyEntityBase. Twas a silly construct.

--- BlogDatalayer/Entities/PostTag.cs
+++ BlogDatalayer/Entities/PostTag.cs
@@ -7,18 +7,12 @@ using System.Threading.Tasks;
 
 namespace BlogDatalayer.Entities
 {
-    public class PostTag : DualPrimaryKeyEntityBase
+    public class PostTag 
     {
         public int PostId { get; set; }
         public int TagId { get; set; }
 
         public virtual Post Post { get; set; }
         public virtual Tag Tag { get; set; }
-
-        public PostTag()
-        {
-            _primaryKey1 = PostId;
-            _primaryKey2 = TagId;
-        }
     }
 }

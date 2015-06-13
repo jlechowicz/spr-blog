1d4aa88d11515d18156ec61c2352fc4b058f2033
John Lechowicz <jlechowicz@gmail.com>
2015-06-13 02:31:25 -0500

    Added User, Role, and UserRole entities

---
+++ BlogDatalayer/Entities/Role.cs
@@ -0,0 +1,15 @@
+﻿using BlogDatalayer.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BlogDatalayer.Entities
+{
+    public class Role : DataEntityBase
+    {
+        public string RoleName { get; set; }
+
+        public virtual ICollection<UserRole> UserRoles { get; set; }
+    }
+}
---
+++ BlogDatalayer/Entities/User.cs
@@ -0,0 +1,23 @@
+﻿using BlogDatalayer.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogDatalayer.Entities
+{
+    public class User : DataEntityBase
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Salt { get; set; }
+        public bool IsLocked { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime? LastModifiedOn { get; set; }
+
+        public virtual ICollection<UserRole> UserRoles { get; set; }
+    }
+}
